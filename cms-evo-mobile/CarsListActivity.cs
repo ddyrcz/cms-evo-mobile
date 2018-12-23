cd4e5bda6e4fe5981c82105b32c4d93f250472d9
Dawid Dyrcz <dawid.dyrcz@iteo.com>
2018-12-23 15:51:05 +0100

    Initial cars list view

---
+++ cms-evo-mobile/CarsListActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace cms_evo_mobile
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class CarsListActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.cars_list);
+
+            var cars = new List<CarModel>
+            {
+                new CarModel("Ford Fusion"),
+                new CarModel("Ford C-MAX"),
+                new CarModel("Audo A6")
+            };
+
+            var carsList = FindViewById<ListView>(Resource.Id.carsList);
+
+            carsList.Adapter = new CarsListAdapter(this, cars);
+
+        }
+    }
+}
---
+++ cms-evo-mobile/CarsListAdapter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace cms_evo_mobile
+{
+    class CarModel
+    {
+        public string Make { get; }
+
+        public CarModel(string make)
+        {
+            Make = make;
+        }
+    }
+
+    class CarsListAdapter : BaseAdapter<CarModel>
+    {
+        private readonly Activity _context;
+        private readonly List<CarModel> _cars;
+
+        public CarsListAdapter(Activity context, List<CarModel> cars)
+            : base()
+        {
+            _context = context;
+            _cars = cars;
+        }
+
+        public override CarModel this[int position] => _cars[position];
+
+        public override int Count => _cars.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = _cars[position];
+            View view = convertView;
+            if (view == null) // no view to re-use, create new
+                view = _context.LayoutInflater.Inflate(Resource.Layout.car_list_item, null);
+            view.FindViewById<TextView>(Resource.Id.list_item_car_make).Text = item.Make;
+            return view;
+        }
+    }
+}
