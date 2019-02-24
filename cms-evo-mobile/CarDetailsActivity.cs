a810da231fa6468f8a8bf387087b82c9e5257f55
Dawid Dyrcz <dawid.dyrcz@iteo.com>
2019-02-24 15:41:42 +0100

    Added date pickers for details

--- cms-evo-mobile/CarDetailsActivity.cs
+++ cms-evo-mobile/CarDetailsActivity.cs
@@ -10,6 +10,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Cms.Utils;
+using Utils;
 using static Cms.Data.ListViewDataStore;
 
 namespace Cms
@@ -17,6 +18,9 @@ namespace Cms
     [Activity(Label = "CarDetailsActivity")]
     public class CarDetailsActivity : Activity
     {
+        TextView _registrationNumberOcExpiry;
+        TextView _registrationNumberAcExpiry;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -33,6 +37,34 @@ namespace Cms
 
             var registrationNumberTextView = FindViewById<TextView>(Resource.Id.details_car_registration_number);
             registrationNumberTextView.Text = car.RegistrationNumber;
+
+
+            _registrationNumberOcExpiry = FindViewById<TextView>(Resource.Id.details_car_oc_expiry);
+            _registrationNumberOcExpiry.Text = car.OcExpiry.ToString("dd.MM.yyyy");
+            _registrationNumberOcExpiry.Click += RegistrationNumberOcExpiry_Click;
+
+
+            _registrationNumberAcExpiry = FindViewById<TextView>(Resource.Id.details_car_ac_expiry);
+            _registrationNumberAcExpiry.Text = car.OcExpiry.ToString("dd.MM.yyyy");
+            _registrationNumberAcExpiry.Click += RegistrationNumberAcExpiry_Click;
+        }
+
+        private void RegistrationNumberAcExpiry_Click(object sender, EventArgs e)
+        {
+            DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+            {
+                _registrationNumberAcExpiry.Text = time.ToString("dd.MM.yyyy");
+            });
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
+        }
+
+        private void RegistrationNumberOcExpiry_Click(object sender, EventArgs e)
+        {            
+            DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+            {
+                _registrationNumberOcExpiry.Text = time.ToString("dd.MM.yyyy");
+            });
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
         }
     }
 }
